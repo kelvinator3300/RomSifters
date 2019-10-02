795d093a26fb45c5348d937f342f2e29f5638b0d
kelvinator3300 <52052613+kelvinator3300@users.noreply.github.com>
2019-10-01 22:22:50 -0500

    Add files via upload

---
+++ batch-files/mame2003plus/002 MOVE MT_NSS_PC_VS.bat
@@ -0,0 +1,19 @@
+;==============================================
+; Create folder and move Mega-Tech (MegaDrive),
+; Nintendo Super System, PlayerChoice, and
+; Nintendo Versus games into it.
+;==============================================
+
+md MT_NSS_PC_VS
+
+move mt_* .\MT_NSS_PC_VS\
+move nss_* .\MT_NSS_PC_VS\
+move pc_* .\MT_NSS_PC_VS\
+
+move nss.zip .\MT_NSS_PC_VS\
+move playch10.zip .\MT_NSS_PC_VS\
+move vsgradus.zip .\MT_NSS_PC_VS\
+move vsgshoe.zip .\MT_NSS_PC_VS\
+move vsnetscr.zip .\MT_NSS_PC_VS\
+move vspingal.zip .\MT_NSS_PC_VS\
+move vsskykid.zip .\MT_NSS_PC_VS\
---
+++ batch-files/mame2003plus/999 MOVE MINIGAMES.bat
@@ -0,0 +1,16 @@
+md MINIGAMES
+move bishi.zip .\MINIGAMES\
+move daiskiss.zip .\MINIGAMES\
+move ginkun.zip .\MINIGAMES\
+move gotcha.zip .\MINIGAMES\
+move ichidant.zip .\MINIGAMES\
+move ichidntb.zip .\MINIGAMES\
+move ichidnte.zip .\MINIGAMES\
+move multchmp.zip .\MINIGAMES\
+move multchmpk.zip .\MINIGAMES\
+move oisipuzl.zip .\MINIGAMES\
+move sotsugyo.zip .\MINIGAMES\
+move tantr.zip .\MINIGAMES\
+move tantrbl2.zip .\MINIGAMES\
+move tantrbl.zip .\MINIGAMES\
+move triplfun.zip .\MINIGAMES\
